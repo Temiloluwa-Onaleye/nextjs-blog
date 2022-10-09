02fdfeeafcb6b3cf0e0a95bf7efdfd777cfd25d3
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-10-09 19:03:21 +0100

    next

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Temiloluwa Onaleye";
+const name = "Temiloluwa Onaley";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
